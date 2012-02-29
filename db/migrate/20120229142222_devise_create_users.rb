4c275820340a28b1d25581dae1b5c948e446a553
huseyin ozcan <huseyinozcan@msn.com>
2012-02-29 22:42:01 +0200

    drop

--- db/migrate/20120229142222_devise_create_users.rb
+++ db/migrate/20120229142222_devise_create_users.rb
@@ -1,5 +1,6 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
+    drop_table :users
     create_table(:users) do |t|
       ## Database authenticatable
       t.string :email,              :null => false, :default => ""
