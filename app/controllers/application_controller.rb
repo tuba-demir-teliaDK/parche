c3152074a981d13b5ef3b0b709ddead31f1d7a50
huseyin ozcan <huseyinozcan@msn.com>
2012-02-29 22:08:43 +0200

    s

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
  
  before_filter :mailer_set_url_options
  
- #db/*.sqlite3
+
   def mailer_set_url_options
     ActionMailer::Base.default_url_options[:host] = request.host_with_port
   end
