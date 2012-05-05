febd94e222f102dfa81ca41fdb94af1654ba2883
huseyinozcan <huseyinozcan@gmail.com>
2012-05-05 17:47:46 +0300

    latest add column

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,13 @@ class ApplicationController < ActionController::Base
  protect_from_forgery 
  before_filter :mailer_set_url_options
  before_filter :authenticate_user!
- 
+ before_filter :reset_sessions
+  
+  def reset_sessions
+    if params[:auth_token]
+      reset_session
+    end
+  end
  
   def mailer_set_url_options
     ActionMailer::Base.default_url_options[:host] = request.host_with_port
